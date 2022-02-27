76e1a9ca1c8976bc92deb00fcfbd4bc7177fa860
geekbrains <TymanRia@gmail.com>
2022-02-27 19:53:58 +0300

    Добавьте файлы проекта.

---
+++ Lesson-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Привет Игорь, сегодня 27.02.2022");
+        }
+    }
+}
